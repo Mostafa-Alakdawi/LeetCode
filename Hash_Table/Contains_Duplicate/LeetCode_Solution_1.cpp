c180132f90182633de109a0997bf69bf281a38fa
Mostafa Alakdawi <mostafa.alakdawi@outlook.com>
2020-11-02 00:01:12 +0200

    Adding the starting code for the coding problme |Contains Duplicate|

---
+++ Hash_Table/Contains_Duplicate/LeetCode_Solution_1.cpp
@@ -0,0 +1,11 @@
+/*
+ * This code runs on LeetCodde playground with no problems
+ * But can't be executed on local machines without making a few changes
+ */
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+    }
+};
